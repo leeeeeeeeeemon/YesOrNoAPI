c2c3f99b2c9d13bebf79bb62282b486f4c05681f
Эмиль Барышев <emilbarish@gmail.com>
2021-06-13 16:17:21 +0300

    Add project files.

---
+++ YesOrNoAPI/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using System.Threading;
+
+namespace YesOrNoAPI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine("Ask a question in your head,i will give you time to think");
+            for(int i = 0; i < 10; i++)
+			{
+                Thread.Sleep(400);
+                Console.WriteLine(".");
+            }
+            Console.WriteLine("the clouds say: " + Answer());
+        }
+            
+
+        static public string Answer()
+        {
+            var url = $"https://yesno.wtf/api";
+            var request = WebRequest.Create(url);
+
+            var response = request.GetResponse();
+            var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+            if (httpStatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine(httpStatusCode);
+            }
+
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                var answer = JsonConvert.DeserializeObject<Root>(result);
+                return (answer.answer);
+            }
+        }
+    }
+}
---
+++ YesOrNoAPI/Root.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace YesOrNoAPI
+{
+	class Root
+	{
+		public string answer { get; set; }
+		public bool forced { get; set; }
+
+		public string image { get; set; }
+
+	}
+}
