4b6d6e4c4ebcaaf8932956771062a6a52ad3934a
Student <Student@DESKTOP-DMNFISP>
2021-06-18 12:51:19 +0300

    add telegram bot code

---
+++ YesOrNoAPI/telegramBot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Telegram.Bot;
+
+namespace TelegramBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TelegramBotClient bot = new TelegramBotClient("1863056024:AAHHFPGNbFpcjL5An6C3VB0Ms4nU37O7vew");
+
+            bot.OnMessage += (s, arg) =>
+            {
+                Console.WriteLine($"{arg.Message.Chat.FirstName}: {arg.Message.Text}");
+                bot.SendTextMessageAsync(arg.Message.Chat.Id, $"You say: {arg.Message.Text}");
+            };
+
+            bot.StartReceiving();
+
+            Console.ReadKey();
+        }
+    }
+}
